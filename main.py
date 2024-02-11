bc3382186811ec74e92dbca1f7536a2c35de8d0e
root <55k@outlook.com>
2024-02-12 00:26:00 +0800

    限制同名数据集添加

--- main.py
+++ main.py
@@ -143,6 +143,12 @@ async def text_logging(item: Import_Text_Item):
     try:
         start_at = time.time()
         
+        # 在添加之前检查dataset_name是否已存在
+        existing_dataset = session.query(TextMeta).filter(TextMeta.dataset_name == item.dataset_name).first()
+        if existing_dataset:
+            # 如果找到了相同的dataset_name，返回提示信息
+            return {"resultCode": "01", "resultMessage": "已添加同名数据集"}
+        
         # 使用会话添加到text_meta表
         new_text_meta = TextMeta(
             dataset_name=item.dataset_name,
